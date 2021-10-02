9329223721666ec4645c78ee9030a065ee89944d
Greeshma143 <58544366+Greeshma143@users.noreply.github.com>
2021-10-02 20:43:05 +0530

    Add files via upload

---
+++ EXERCISES_6A.sh
@@ -0,0 +1,14 @@
+echo "CURRENTLY LOGGED USER"
+whoami
+echo "----------------------"
+echo "CURRENT SHELL"
+echo $SHELL
+echo "----------------------"
+echo "HOME DIRECTORY"
+echo $HOME
+echo "----------------------"
+echo "OPERATING SYSTEM TYPE"
+cat /etc/os-release | head -n 1
+echo "----------------------"
+echo "CURRENT WORKING DIRECTORY"
+pwd 
