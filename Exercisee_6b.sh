303e09a0b41cbd6310bb45b35c891d8c8b9ee86b
Greeshma143 <58544366+Greeshma143@users.noreply.github.com>
2021-10-07 17:54:18 +0530

    Update Exercisee_6b.sh

--- Exercisee_6b.sh
+++ Exercisee_6b.sh
@@ -1,6 +1,7 @@
 echo ".........OS VERSION,RELEASE NUMBER AND KERNAL VERSION ARE....."
 cat /etc/os-release |head -n 2
 uname -r
+uname -v
 echo ".......ALL AVAILABLE SHELLS......."
 cat /etc/shells
 echo ".......FILE SYSTEM (MOUNTED)......"
